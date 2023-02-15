85bc55a871b41791ac78061432b3680668a9921d
steven <mrjiangyan@hotmail.com>
2023-02-15 23:49:59 +0800

    支持sse的版本

--- service/src/test/java/com/touchbiz/chatgpt/simple/EventStreamTest.java
+++ service/src/test/java/com/touchbiz/chatgpt/simple/EventStreamTest.java
@@ -4,48 +4,81 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.theokanning.openai.OpenAiApi;
+import com.theokanning.openai.OpenAiService;
+import com.theokanning.openai.completion.CompletionRequest;
+import com.touchbiz.common.utils.tools.JsonUtils;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import okhttp3.*;
 import org.junit.Test;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import retrofit2.converter.jackson.JacksonConverterFactory;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class EventStreamTest {
 
-    String token = "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";
+    String token = "";
 
     @Test
     public void testRetrofit(){
+
+        CompletionRequest completionRequest = CompletionRequest.builder()
+//                .prompt("Human:" + chat.prompt +"\nAI:")
+                .prompt("给我推荐10本小说")
+                .model("text-davinci-001")
+//                .echo(true)
+                .stop(Arrays.asList(" Human:"," AI:"))
+                .maxTokens(1024)
+                .presencePenalty(0d)
+                .frequencyPenalty(0d)
+                .temperature(0.7D)
+                .bestOf(1)
+                .topP(1d)
+                .stream(true)
+                .build();
+        OpenAiService service = new OpenAiService(token);
+
+        var result = service.createCompletion(completionRequest);
+        log.info("result:{}", JsonUtils.toJson(result));
+    }
+
+    @SneakyThrows
+    @Test
+    public void testHttp() throws ExecutionException, InterruptedException {
+
+        HttpClient client = HttpClient.newBuilder().build();
+
+        CompletionRequest completionRequest = CompletionRequest.builder()
+//                .prompt("Human:" + chat.prompt +"\nAI:")
+                .prompt("给我推荐10本小说")
+                .model("text-davinci-001")
+//                .echo(true)
+                .stop(Arrays.asList(" Human:"," AI:"))
+                .maxTokens(1024)
+                .presencePenalty(0d)
+                .frequencyPenalty(0d)
+                .temperature(0.7D)
+                .bestOf(1)
+                .topP(1d)
+                .stream(true)
+                .build();
+
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
-        String token = "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";
-        OkHttpClient client = (new OkHttpClient.Builder()).addInterceptor(new AuthenticationInterceptor(token))
-                .connectionPool(new ConnectionPool(5, 1L, TimeUnit.SECONDS)).readTimeout(10*1000, TimeUnit.MILLISECONDS).build();
-        Retrofit retrofit = (new Retrofit.Builder()).baseUrl("https://api.openai.com/").client(client)
-                .addConverterFactory(JacksonConverterFactory.create(mapper))
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create()).build();
-        var api = (OpenAiApi)retrofit.create(OpenAiApi.class);
-    }
 
-    @Test
-    public void testHttp() throws ExecutionException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
-
+        var json = mapper.writeValueAsString(completionRequest);
+        log.info("json:{}", json);
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://example.com/sse"))
+                .header("Authorization", "Bearer " + this.token)
+                .header( "Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .uri(URI.create("https://api.openai.com/v1/completions"))
                 .build();
 
         client.sendAsync(request, HttpResponse.BodyHandlers.ofLines())
@@ -53,16 +86,4 @@ public class EventStreamTest {
                 .forEach(System.out::println);
     }
 
-    public static class AuthenticationInterceptor implements Interceptor {
-        private final String token;
-
-        AuthenticationInterceptor(String token) {
-            this.token = token;
-        }
-
-        public Response intercept(Interceptor.Chain chain) throws IOException {
-            Request request = chain.request().newBuilder().header("Authorization", "Bearer " + this.token).build();
-            return chain.proceed(request);
-        }
-    }
 }
