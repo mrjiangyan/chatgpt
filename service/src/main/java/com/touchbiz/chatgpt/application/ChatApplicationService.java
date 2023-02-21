84277ff4dccdfe169ed0692815195bc567d7fe98
huangtao2406 <huangtao2406@163.com>
2023-02-21 16:25:58 +0800

    新增会话信息、校验会话Id

--- service/src/main/java/com/touchbiz/chatgpt/application/ChatApplicationService.java
+++ service/src/main/java/com/touchbiz/chatgpt/application/ChatApplicationService.java
@@ -3,7 +3,9 @@ package com.touchbiz.chatgpt.application;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.touchbiz.chatgpt.database.domain.ChatSession;
+import com.touchbiz.chatgpt.dto.Chat;
 import com.touchbiz.chatgpt.dto.ChatInfo;
+import com.touchbiz.chatgpt.dto.ChatResult;
 import com.touchbiz.common.entity.model.SysUserCacheInfo;
 
 /**
@@ -20,7 +22,9 @@ public interface ChatApplicationService {
 
 	ChatSession createSession(SysUserCacheInfo sysUser);
 
-	void add(ChatInfo chatInfo, SysUserCacheInfo sysUser);
+	String createSessionInfo(Chat chat, ChatResult result, SysUserCacheInfo sysUser);
 
 	void deleteSession(String id, SysUserCacheInfo user);
+
+	void checkSessionId(String sessionId);
 }
--- service/src/main/java/com/touchbiz/chatgpt/dto/Chat.java
+++ service/src/main/java/com/touchbiz/chatgpt/dto/Chat.java
@@ -2,7 +2,19 @@ package com.touchbiz.chatgpt.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
-public class Chat{
-    String prompt;
+public class Chat {
+    /**
+     *  会话id
+     */
+    @NotBlank(message = "会话id不存在")
+    private String sessionId;
+
+    /**
+     *  问题
+     */
+    @NotBlank(message = "请提问")
+    private String prompt;
 }
